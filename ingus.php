aa128c56259bb0ea83239df98dea66b866ae6426
Nikita Menshutin <nikita.menshutin@edited.com>
2025-03-16 11:14:03 +0200

    chore: rules in json file

--- ingus.php
+++ ingus.php
@@ -7,8 +7,12 @@ date_default_timezone_set( 'Europe/Moscow' );
 define( 'NG_ING_PREFIX', 'NGING' );
 define( 'NG_ING_VERSION', '0.1.0' );
 define( 'NGING_LOGFILE', __DIR__ . '/logs/ingus' . date( 'Ymd' ) . '.log' );
+$rules = json_decode(file_get_contents( __DIR__ . '/rules.json' ), true);
+if ( ! $rules || ! is_array( $rules ) ) {
+	throw new Exception( 'Invalid rules.json' );
+}
+define( 'NGING_REGEX_RULES', $rules );
 require_once 'config.php';
-require_once 'rules.php';
 $bot = new \Ng\Ingus\Controller\Bot();
 $bot->get_updates( array() );
 $bot->filter_updates( NGINS_CHATTS );
@@ -23,12 +27,12 @@ foreach ( $bot->data['updates'] as $update ) {
 	}
 	$guard = new \Ng\Ingus\Controller\Guard( $update );
 	if ( defined( 'NGINS_COPY_ALL' ) && NGINS_COPY_ALL ) {
-		$bot->send_message( NGINS_ADMIN_CHAT, 'Update: ' . $update->getMessage()->getText() . ' from ' . $update->getMessage()->getChat()->getId().' chat name:'.$update->getMessage()->getChat()->getTitle() );
+		$bot->send_message( NGINS_ADMIN_CHAT, 'Update: ' . $update->getMessage()->getText() . ' from ' . $update->getMessage()->getChat()->getId() . ' chat name:' . $update->getMessage()->getChat()->getTitle() );
 
 	}
 	if ( $guard->is_spam( $update ) ) {
-		// $bot->delete_message( $update->getMessage()->getChat()->getId(), $update->getMessage()->getMessageId() );
 		$bot->send_message( NGINS_ADMIN_CHAT, 'Spam detected: ' . $update->getMessage()->getText() );
+		$bot->delete_message( $update->getMessage()->getChat()->getId(), $update->getMessage()->getMessageId() );
 	}
 	$cache->set( NG_ING_PREFIX . '_update_' . $update_id, $update_id, 24 * 60 * 60 );
 }
