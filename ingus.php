43f23ed7dab20ddee09c6197bbeb723926adc2eb
Nikita Menshutin <nikita.menshutin@edited.com>
2025-03-17 16:33:52 +0200

    chore: reverted updates caching

--- ingus.php
+++ ingus.php
@@ -28,7 +28,7 @@ foreach ( $bot->data['updates'] as $update ) {
 	if ( $cache->get( NG_ING_PREFIX . '_update_' . $update_id ) ) {
 		continue;
 	}
-	// $cache->set( NG_ING_PREFIX . '_update_' . $update_id, $update_id, 24 * 60 * 60 );
+	$cache->set( NG_ING_PREFIX . '_update_' . $update_id, $update_id, 24 * 60 * 60 );
 	$date        = $update?->getMessage()?->getDate() ?? '';
 	$update_info = $bot->get_update_info( $update );
 	$guard       = new \Ng\Ingus\Controller\Guard( $update );
@@ -37,7 +37,7 @@ foreach ( $bot->data['updates'] as $update ) {
 		$bot->restrict_chat_member( $update->getMessage()->getChat()->getId(), $update->getMessage()->getFrom()->getId() );
 		$bot->delete_message( $update->getMessage()->getChat()->getId(), (int) $update->getMessage()->getMessageId() );
 	} elseif ( defined( 'NGINS_COPY_ALL' ) && NGINS_COPY_ALL ) {
-			send_message( NGINS_ADMIN_CHAT, 'New message: ' . "\n" . $update_info );
+		send_message( NGINS_ADMIN_CHAT, 'New message: ' . "\n" . $update_info );
 	}
 }
 
