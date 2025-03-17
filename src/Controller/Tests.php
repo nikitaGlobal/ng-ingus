5677d5c9d2cbd6228e0423d3d00478a8f0342d6a
Nikita Menshutin <nikita.menshutin@edited.com>
2025-03-17 13:02:50 +0200

    chore: skip null in Tests

--- src/Controller/Tests.php
+++ src/Controller/Tests.php
@@ -5,6 +5,9 @@ namespace Ng\Ingus\Controller;
 trait Tests {
 
 	public function test_regex( $text ) {
+		if ( ! is_string( $text ) ) {
+			return true;
+		}
 		foreach ( NGING_REGEX_RULES as $pattern ) {
 			if ( 1 === preg_match( $pattern, $text ) ) {
 				return true;
